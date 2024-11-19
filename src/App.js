957176949e25859624f73f78cb99d525ccee4131
dominikdobos <dominik.dobos33@gmail.com>
2024-11-19 10:06:51 +0100

    Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container">
+      <header></header>
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="public" element={<Public />} />
+              <Route path="admin" element={<Admin />} />
+              <Route path="*" element={<NoPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </div>
+  );
 }
 
 export default App;
