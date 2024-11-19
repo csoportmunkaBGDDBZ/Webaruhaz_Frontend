0b0bea553aecdd1b2755add3cd367037ec317b54
Bito-Zalan <bitozalan23@gmail.com>
2024-11-19 10:11:35 +0100

    admin tablazat elkezdve

---
+++ src/components/TablazatAdmin.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function TablazatAdmin() {
+  return (
+    <>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col1">ID</th>
+            <th scope="col1">Title</th>
+            <th scope="col1">Price</th>
+            <th scope="col1">Description</th>
+            <th scope="col1">Category</th>
+          </tr>
+        </thead>
+      </table>
+    </>
+  );
+}
+
+export default TablazatAdmin;
